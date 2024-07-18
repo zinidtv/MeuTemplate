b26287b768e0b4a50c74ee7bb0ab785e7d99f9a1
Alexandre Diniz <zinidgames@gmail.com>
2024-07-18 19:16:23 -0300

    .

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+const url = "https://www.amazon.com.br/Siracusa-Guilherme-Sá-ebook/dp/B09ZMKJLD1?ref_=nav_signin"
+const btn = document.querySelector("#btn")
+
 function clickMenu() {
     let menuMobile = document.querySelector(".mobile-menu");
     if (menuMobile.classList.contains("open")) {
@@ -8,3 +11,12 @@ function clickMenu() {
         document.querySelector(".icon").src = "midias/close.svg";
     }
 }
+
+function openInNewTab(url) {
+    const win = window.open(url, "_blank")
+    win.focus()
+}
+
+btn.addEventListener("click", () => {
+    openInNewTab(url)
+})
